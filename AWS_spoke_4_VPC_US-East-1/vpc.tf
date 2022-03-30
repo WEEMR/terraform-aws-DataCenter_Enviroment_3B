619e826a75260e643be14c7d356a4ef1b91ecce6
waleedrady <82145296+waleedrady@users.noreply.github.com>
2022-03-30 14:48:13 -0400

    Add files via upload

---
+++ AWS_spoke_4_VPC_US-East-1/vpc.tf
@@ -0,0 +1,32 @@
+##############################################################################################################
+# Production VPC - spoke 4
+##############################################################################################################
+resource "aws_vpc" "SDWAN_VPC" {
+  provider             = aws.spoke_4_vpc
+  cidr_block           = var.spoke_4_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name            = "${var.username}_Terraform_spoke_4_Prod_VPC"
+    Resources_Owner = var.username
+    Enviroment      = "Terraform Production"
+  }
+}
+
+// Get all avaliable AZ's in the region variable
+
+data "aws_availability_zones" "AZs" {
+  provider = aws.spoke_4_vpc
+  state = "available"
+}
+
+# IGW
+resource "aws_internet_gateway" "prod_vpc_igw" {
+  provider       = aws.spoke_4_vpc
+  vpc_id         = aws_vpc.SDWAN_VPC.id
+  tags = {
+    Name            = "${var.username}_Terraform_spoke_4_Prod_VPC_IGW"
+    Resources_Owner = var.username
+    Enviroment      = "Terraform Testing"
+  }
+}
